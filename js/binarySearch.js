0cc333bd06f51ae278044310f10821b70b4a893a
JohnOK <john.ok.esp@gmail.com>
2022-06-10 00:13:22 +0200

    Added JS spec file.

--- js/binarySearch.js
+++ js/binarySearch.js
@@ -1,4 +1,5 @@
-function binarySearch(num, array){
+// exports.function binarySearch(num, array) {
+exports.binarySearch = function(num, array) {
   array.sort();
   let lower = 0;
   let middle = Math.floor(array.length / 2);
@@ -19,13 +20,3 @@ function binarySearch(num, array){
 
   return -1
 }
-
-var smallArray = [1,2,3,4,5]
-var largeArray = [1,5,7,2,3,8,4,9]
-
-console.log(binarySearch(1, smallArray) === 0);
-console.log(binarySearch(2, smallArray) === 1);
-console.log(binarySearch(3, smallArray) === 2);
-console.log(binarySearch(4, smallArray) === 3);
-console.log(binarySearch(5, smallArray) === 4);
-console.log(binarySearch(7, largeArray) === 5);
--- js/binarySearchSpec.js
+++ js/binarySearchSpec.js
@@ -1,1 +1,11 @@
-// Write your unit tests here
+var bs = require("./binarySearch");
+
+var smallArray = [1,2,3,4,5]
+var largeArray = [1,5,7,2,3,8,4,9]
+
+console.log(bs.binarySearch(1, smallArray) === 0);
+console.log(bs.binarySearch(2, smallArray) === 1);
+console.log(bs.binarySearch(3, smallArray) === 2);
+console.log(bs.binarySearch(4, smallArray) === 3);
+console.log(bs.binarySearch(5, smallArray) === 4);
+console.log(bs.binarySearch(7, largeArray) === 5);
