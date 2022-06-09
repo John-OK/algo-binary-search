e983fc99840a2d69ecd3d64c83713042d74f9d07
JohnOK <john.ok.esp@gmail.com>
2022-06-09 23:59:07 +0200

    Added JS version of binary search.

--- js/binarySearch.js
+++ js/binarySearch.js
@@ -1,5 +1,23 @@
-function binarySearch(){
-  // Code goes here!
+function binarySearch(num, array){
+  array.sort();
+  let lower = 0;
+  let middle = Math.floor(array.length / 2);
+  let upper = array.length - 1;
+
+  while (lower <= upper) {
+    let middle_val = array[middle];
+    if (num == middle_val) {
+      return middle;
+    } else if (num > middle_val) {
+      lower = middle + 1;
+      middle = Math.floor((upper - lower) / 2) + lower;
+    } else {
+      upper = middle - 1;
+      middle = Math.floor((upper - lower) / 2) + lower;
+    }
+  }
+
+  return -1
 }
 
 var smallArray = [1,2,3,4,5]
